5af50e32caa301004c411c7b4e693352356850e6
Cheng Vue <csalvue@gmail.com>
2019-03-08 02:07:53 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
   def upload
     CSV.foreach(params[:file].path, headers: true) do |song|
       Song.create(title: song[0])
-      Artist.Create(name: song[1])
+      Artist.create(name: song[1])
     end
     redirect_to customers_path
   end
